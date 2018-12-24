6ccc4acdf641fd24634254505373bda6c3342028
Shardul <shardul17399@gmail.com>
2018-12-24 13:05:00 +0530

    Addition of label and button creating functions

--- src/userInterface/HomePage.java
+++ src/userInterface/HomePage.java
@@ -11,22 +11,43 @@ public class HomePage {
 	
 	private JButton searchButton, insertButton, deleteButton, updateButton;
 	
-	private JLabel idLabel, nameLabel, descLabel, locLabel, remLabel, yearLabel, collectLabel, ownLabel, catLabel;
+	private JLabel idLabel, nameLabel, descLabel, locLabel, remarkLabel, yearLabel, collectLabel, ownerLabel, categoryLabel;
+	
+	private JPanel labelPanel, buttonPanel;
 	
 	private void setButtons() {
 		searchButton=new JButton("Search");
 		insertButton=new JButton("Insert");
+		deleteButton=new JButton("Delete");
+		updateButton=new JButton("Update");
 	}
 	
 	private void setLabels() {
+		idLabel=new JLabel("ID");
+		nameLabel=new JLabel("Name");
+		descLabel=new JLabel("Description");
+		locLabel=new JLabel("Location");
+		remarkLabel=new JLabel("Remark");
+		yearLabel=new JLabel("Year");
+		collectLabel=new JLabel("Collection");
+		ownerLabel=new JLabel("Owner");
+		categoryLabel=new JLabel("Category");
+	}
+	
+	private void setPanel() {
 		
 	}
 	
 	public HomePage() {
 		homePageFrame=new JFrame();
 		frameLayout=new GridLayout(3,1,0,5);
+		labelPanel=new JPanel();
+		labelPanel.setLayout(new GridLayout(1,9));
+		buttonPanel=new JPanel();
+		buttonPanel.setLayout(new GridLayout(1,4));
 		setButtons();
 		setLabels();
+		setPanel();
 	}
 	
 }
